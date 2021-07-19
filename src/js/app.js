c93d7516a31384f9627ae404f283764132476d11
chery-pynya <nikita.cherepnya@gmail.com>
2021-07-19 23:27:08 +0300

    deploy

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import Bot from './bot';
 
-const heroku = 'https://cherry-pynya-ahj-diploma-back.herokuapp.com';
+const heroku = 'ws://cherry-pynya-ahj-diploma-back.herokuapp.com';
 // const localhost = 'ws://localhost:7777';
 
 window.bot = new Bot('.bot-container', heroku);
