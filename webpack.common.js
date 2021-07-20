b7a4140e85fef2c5b0bda7771c188afebda72641
chery-pynya <nikita.cherepnya@gmail.com>
2021-07-21 00:05:07 +0300

    webpak

--- webpack.common.js
+++ webpack.common.js
@@ -23,9 +23,16 @@ module.exports = {
           },
         ],
       },
+      { test: /\.css$/, 
+        use: [ 'style-loader', 'css-loader' ] 
+      },
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       },
       {
         test: /\.(png|jpg|gif)$/i,
